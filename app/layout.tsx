48f8991707044b2257f7cd48f2b8c8c5a18af113
Frost <mathis@mbp-de-mathis.home>
2024-08-19 21:37:08 +0200

    error page edited againn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
+"use server";
 import Header from "@/components/landing/Header";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/ui/themeProvider";
-import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
 
 import Footer from "@/components/landing/Footer";
 import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "F'Threads",
-  description: "F'Threads is a social media platform.",
-};
 
 export default async function RootLayout({
   children,
