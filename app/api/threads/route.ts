c85979c807d8c3dfcb7ae18b09e293cf85980840
Frost <mathis@macbook-pro-de-mathis.home>
2024-09-01 17:04:45 +0200

    fixed req and res types on api route

--- app/api/threads/route.ts
+++ app/api/threads/route.ts
@@ -1,8 +1,7 @@
 import { prisma } from "@/lib/prisma";
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: Request, res: Response) {
   try {
     const { searchParams } = new URL(req.url as string);
     const page = searchParams.get("page") as string;
